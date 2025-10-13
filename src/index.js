f495c310c72b76beb3a5ce1045149cd1aa1a5a78
pociello <asierpociello@gmail.com>
2025-10-13 08:18:53 +0200

    Console.log WeatherData from API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./style.css";
 
-console.log("Template Repository");
+async function getWeather() {
+    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/madrid?unitGroup=metric&include=hours&key=T7W7PCFE2UAPXW6CZ6ZUNY9HV&contentType=json");
+    const weatherData = await response.json();
+
+    console.log(weatherData);
+}
+
+
+getWeather()
