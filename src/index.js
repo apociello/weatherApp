c2802a43f18887cfa40553511cb094d3d3ed78f3
pociello <asierpociello@gmail.com>
2025-10-13 09:01:32 +0200

    Console.log following weather info: - Date - Max/Min/Current temperature - Precipitation % - Humidity % - Wind km/h

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ async function getWeather() {
     const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/madrid?unitGroup=metric&include=hours&key=T7W7PCFE2UAPXW6CZ6ZUNY9HV&contentType=json");
     const weatherData = await response.json();
 
-    console.log(weatherData);
-}
+    const currentHour = new Date().getHours();
 
+    console.log(`Date: ${weatherData.days[0].datetime}`);
+    console.log(`Max: ${weatherData.days[0].tempmax}º`);
+    console.log(`Min: ${weatherData.days[0].tempmin}º`);
+    console.log(`Date: ${weatherData.days[0].datetime}`);
+    
+    console.log(`Temp: ${weatherData.days[0].hours[8].temp}º`);
+    console.log(`Precipitation: ${weatherData.days[0].hours[8].precipprob}%`);
+    console.log(`Humidity: ${weatherData.days[0].hours[currentHour].humidity}%`);
+    console.log(`Wind: ${weatherData.days[0].hours[currentHour].windspeed} km/h`)
+}
 
 getWeather()
