cd7ee4f77305188c4c6ae463524438effdbb21db
pociello <asierpociello@gmail.com>
2025-10-14 07:51:29 +0200

    Add a form that let's the user fetch a city: - The info is loged in the console

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./style.css";
 
-async function getWeather() {
-    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/madrid?unitGroup=metric&include=hours&key=T7W7PCFE2UAPXW6CZ6ZUNY9HV&contentType=json");
+
+async function getWeather(city) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&include=hours&key=T7W7PCFE2UAPXW6CZ6ZUNY9HV&contentType=json`);
     const weatherData = await response.json();
 
     const currentHour = new Date().getHours();
@@ -9,12 +10,19 @@ async function getWeather() {
     console.log(`Date: ${weatherData.days[0].datetime}`);
     console.log(`Max: ${weatherData.days[0].tempmax}º`);
     console.log(`Min: ${weatherData.days[0].tempmin}º`);
-    console.log(`Date: ${weatherData.days[0].datetime}`);
     
     console.log(`Temp: ${weatherData.days[0].hours[8].temp}º`);
     console.log(`Precipitation: ${weatherData.days[0].hours[8].precipprob}%`);
     console.log(`Humidity: ${weatherData.days[0].hours[currentHour].humidity}%`);
     console.log(`Wind: ${weatherData.days[0].hours[currentHour].windspeed} km/h`)
-}
+};
 
-getWeather()
+const searchBtn = document.querySelector('.search');
+const cityInput = document.querySelector('.city');
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const city = cityInput.value;
+    getWeather(city);
+});
+
+getWeather('madrid');
