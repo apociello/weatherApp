2ace4a102a184c8bb8a2733751121f74522d71f2
pociello <asierpociello@gmail.com>
2025-10-16 08:53:39 +0200

    Add functionality to °C/°F buttons: - °C btn: changes units to °C & km/h - °F btn: changes units to °F & mph

--- src/index.js
+++ src/index.js
@@ -17,26 +17,23 @@ const max = document.querySelector('.max');
 const precipitation = document.querySelector('.precipitation');
 const humidity = document.querySelector('.humidity');
 const wind = document.querySelector('.wind');
+const cityTitle = document.querySelector('.city-title');
+let unit = 'metric';
+let tempUnit;
+let windUnit;
+
 
 async function getWeather(city) {
 
     try {
 
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&include=hours&key=T7W7PCFE2UAPXW6CZ6ZUNY9HV&contentType=json`);
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unit}&include=hours&key=T7W7PCFE2UAPXW6CZ6ZUNY9HV&contentType=json`);
         if (!response.ok) {
             throw new Error(`Error: ${response.status}`);
         }
 
         const weatherData = await response.json();
         const currentHour = new Date().getHours();
-
-        date.textContent = `Date: ${weatherData.days[0].datetime}`;
-        max.textContent = `Max: ${Math.round(weatherData.days[0].tempmax)}`
-        min.textContent = `Min:${Math.round(weatherData.days[0].tempmin)}`
-        temperature.textContent = Math.round(weatherData.days[0].hours[currentHour].temp);
-        precipitation.textContent = `Precipitation: ${Math.round(weatherData.days[0].hours[currentHour].precipprob)}%`;
-        humidity.textContent = `Humidity: ${Math.round(weatherData.days[0].hours[currentHour].humidity)}%`;
-        wind.textContent = `Wind: ${weatherData.days[0].hours[currentHour].windspeed} km/h`;
 
         const icon = weatherData.days[0].hours[currentHour].icon;
         if (icon === 'clear-day') {
@@ -59,7 +56,26 @@ async function getWeather(city) {
             weatherIcon.src = clearDay;
         }
 
-        console.log(weatherData.days[0].hours[currentHour].icon);
+
+        if (unit === 'metric') {
+            tempUnit = '°C';
+            windUnit = 'km/h'
+
+        } else if ( unit === 'us') {
+            tempUnit = '°F';
+            windUnit = 'mph'
+        }
+
+        cityTitle.textContent = city.toUpperCase();
+        date.textContent = `Date: ${weatherData.days[0].datetime}`;
+        max.textContent = `Max: ${Math.round(weatherData.days[0].tempmax)} ${tempUnit}`
+        min.textContent = `Min: ${Math.round(weatherData.days[0].tempmin)} ${tempUnit}`
+        temperature.textContent = Math.round(weatherData.days[0].hours[currentHour].temp);
+        precipitation.textContent = `Precipitation: ${Math.round(weatherData.days[0].hours[currentHour].precipprob)}%`;
+        humidity.textContent = `Humidity: ${Math.round(weatherData.days[0].hours[currentHour].humidity)}%`;
+        wind.textContent = `Wind: ${weatherData.days[0].hours[currentHour].windspeed} ${windUnit}`;
+
+        
 
     } catch (error) {
             console.log(error);
@@ -73,6 +89,19 @@ form.addEventListener('submit', (e) => {
     const city = cityInput.value.trim();
     getWeather(city);
     cityInput.value = '';
+});
+
+const fahrenheitBtn = document.getElementById('us');
+const celciusBtn = document.getElementById('metric');
+
+fahrenheitBtn.addEventListener('click', () => {
+    unit = 'us';
+    getWeather(cityTitle.textContent);
+});
+
+celciusBtn.addEventListener('click', () => {
+    unit = 'metric';
+    getWeather(cityTitle.textContent);
 });
 
 getWeather('madrid');
